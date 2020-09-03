83281362ab131be6cdb90e6bf86ea3db8c1b5065
Your Name <clonazepam24@gmail.com>
2020-09-03 12:11:29 -0300

    Abc

---
+++ Boligrafo/Boligrafo/Boligrafo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boligrafo
+{
+    class Boligrafo
+    {
+
+        const int cantidadTintaMaxima = 100;
+        ConsoleColor color;
+        int tinta;
+
+        public Boligrafo(ConsoleColor color , int tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor GetColor()
+        {
+
+            return this.color;
+        }
+
+        public int getTinta()
+        {
+            return this.tinta;
+        }
+
+        private bool setTinta(int tinta)
+        {
+            if(tinta>=0 && tinta<=cantidadTintaMaxima)
+            {
+                this.tinta -= tinta;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("fuera de los parametros posibles");
+                return false;
+            }
+            
+        }
+
+        public void Recargar()
+        {
+            this.tinta = cantidadTintaMaxima;
+        }
+
+        public bool Pintar(int gasto , out string mensaje)
+        {
+            bool retorno ;
+            float porcentaje = 0;
+            float cuantoQueda = 0;
+            float tinta = this.tinta;
+
+            if (setTinta(gasto))
+            {
+                
+                porcentaje = ((float)gasto * tinta) / 100;
+                cuantoQueda = (float)100-porcentaje;
+
+              //  mensaje = $"se gasto un total de {gasto} , quedando la tinta en {this.tinta}\n\n";
+                mensaje = $"Nivel de tinta gastado: %{porcentaje:0.00}\n[";
+                mensaje += new string('*', (int)porcentaje);
+                mensaje += $"]\n\n";
+                mensaje +=  $"Nivel de Tinta: %{cuantoQueda:0.00}\n[";
+                mensaje += new string('*', (int)this.tinta);
+                mensaje += $"]";
+                retorno = true;
+            }
+            else
+            {
+                mensaje = "no se puede gastar mas de la tinta posible.";
+                retorno = false;
+            }
+
+
+           
+            return retorno;
+        }
+
+
+    }
+}
---
+++ Boligrafo/Boligrafo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boligrafo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensaje;
+            int gasto = 110;
+
+            Boligrafo BoligrafoRojo = new Boligrafo(ConsoleColor.Red, 100);
+
+            Boligrafo BoligrafoAzul = new Boligrafo(ConsoleColor.Blue, 100);
+
+            BoligrafoRojo.Recargar();
+
+            BoligrafoAzul.Recargar();
+
+            Console.WriteLine($"El total de tinta del boligrafo rojo es de %{BoligrafoRojo.getTinta()}\n" +
+                $"se va a utilizar un total de %{gasto}\n");
+
+
+
+            BoligrafoRojo.Pintar(gasto, out mensaje);
+
+            Console.WriteLine(mensaje);
+
+            Console.ReadKey();
+        }
+    }
+}
